5773e5eaac95dfdf4aad35206687f147956edec5
pidewap <pidewap@gmail.com>
2018-09-13 10:11:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $password = "pidewap321";
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,"http://exnaid.com/login.html");
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, "email=" .$username. "&password=" .$password);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "email=" .$username. "&password=" .$password. "&naelogin=Masuk");
 curl_exec ($curl);
 curl_close ($curl);
 ?>
