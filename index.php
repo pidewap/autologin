0f415bf6dc7047c46a20250bb3775f7473433d03
pidewap <pidewap@gmail.com>
2018-09-13 10:09:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_URL,"http://peed.wapka.mobi/login_site.xhtml?do_id=0&wu_login=lagu&wu_heslo=pidewap321&wu_autologin=1&log_submit=Log+in&hash=6780ed6b82719c825f4668c0d18739");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie-name.txt');  
-curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie-name.txt');
-$hasil=curl_exec($ch);
-curl_close ($ch);
-echo $hasil;
+$username = "pidewap@gmail.com";
+$password = "pidewap321";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL,"http://exnaid.com/login.html");
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "email=" .$username. "&password=" .$password);
+curl_exec ($curl);
+curl_close ($curl);
 ?>
